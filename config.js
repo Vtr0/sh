2a2b5c3e13238002e58e2b5c4da252ba321fa85a
Vtr0 <icwt@rocketmail.com>
2024-10-24 17:02:43 +0700

    Correct default speed and volume

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const _DEFAULT_CFG = {xState: {speed:1.3,volume:.9,history:{currHis:0,hisList:[]}},
+const _DEFAULT_CFG = {xState: {speed:1.4,volume:1,history:{currHis:0,hisList:[]}},
 xDBFilesCfg: {len:3,toLoad:null}};
 const ssData = {
 	wildCartStr: "<*~~*>",
