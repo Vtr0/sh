7368eb50f267d801724689b5265aff6615159a36
Vtr0 <icwt@rocketmail.com>
2024-10-24 14:08:55 +0700

    Update config.js - add speed, volume attribute

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const _DEFAULT_CFG = {xState: {"history":{"currHis":0,"hisList":[]}},
-xDBFilesCfg: {"len":3,"toLoad":null}};
+const _DEFAULT_CFG = {xState: {speed:1.3,volume:.9,history:{currHis:0,hisList:[]}},
+xDBFilesCfg: {len:3,toLoad:null}};
 const ssData = {
 	wildCartStr: "<*~~*>",
-	xBookGrp: [{"grpWay":"Nhóm theo Bộ truyện",grp:[]},{"grpWay":"Sắp xếp theo Người đọc",grp:[]},{"grpWay":"Sắp xếp theo Tên truyện",grp:[]}]
+	xBookGrp: [{grpWay:"Nhóm theo Bộ truyện",grp:[]},{grpWay:"Sắp xếp theo Người đọc",grp:[]},{grpWay:"Sắp xếp theo Tên truyện",grp:[]}]
 }
