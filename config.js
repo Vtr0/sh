47e924afb3c03f6b42be118d23e4e9dc21207898
Vtr0 <icwt@rocketmail.com>
2024-10-27 09:17:38 +0700

    Fix error of config.js that I forgot to change

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-const _DEFAULT_CFG = {xState: {speed:1.4,volume:1,history:{currHis:0,hisList:[]}},
-cDBFilesEdt: {checkEdtDelay:6400, edt:[101,101,101]}, //43200, 86400
-xDBFilesCfg: {len:3,toLoad:null}};
-const ssData = {
+var _DEFAULT_CFG = {xState: {speed:1.4,volume:1,history:{currHis:0,hisList:[]}},
+cDBFilesEdt: {checkEdtDelay:6400, edt:[101,101,101]}, //43200,86400
+xDBFilesCfg: {len:3,toLoad:null}}; 
+var ssData = ssData || {
 	wildCartStr: "<*~~*>",
 	xBookGrp: [{grpWay:"Nhóm theo Bộ truyện",grp:[]},{grpWay:"Sắp xếp theo Người đọc",grp:[]},{grpWay:"Sắp xếp theo Tên truyện",grp:[]}]
 }
