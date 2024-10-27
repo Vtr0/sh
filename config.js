8c7a797504655d92e173103216909688d777478f
Vtr0 <icwt@rocketmail.com>
2024-10-27 09:13:12 +0700

    Update config.js format to support checking DB update functionality

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const _DEFAULT_CFG = {xState: {speed:1.4,volume:1,history:{currHis:0,hisList:[]}},
+cDBFilesEdt: {checkEdtDelay:6400, edt:[101,101,101]}, //43200, 86400
 xDBFilesCfg: {len:3,toLoad:null}};
 const ssData = {
 	wildCartStr: "<*~~*>",
